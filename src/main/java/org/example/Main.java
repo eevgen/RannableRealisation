ec799c0b37e3016f804aefb0d7051b6e249c42c7
eevgen <evgeniy.kalenichenko@gmail.com>
2023-07-22 19:17:42 +0300

    Add Lock() synchronization

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,20 @@
 package org.example;
 
 import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
     public static void main(String[] args) {
+        Lock lock = new ReentrantLock();
         final Runnable task = () -> {
-            for (int i = 0; i < (new Random().nextInt(100, 500)); i++) {
-                System.out.println(Thread.currentThread().getName() + ": " + i);
+            try {
+                lock.lock();
+                for (int i = 0; i < (new Random().nextInt(100, 500)); i++) {
+                    System.out.println(Thread.currentThread().getName() + ": " + i);
+                }
+            } finally {
+                lock.unlock();
             }
         };
         final Thread thread1 = new Thread(task);
